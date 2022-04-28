7e7a8a6cfbd1f3b659c0a5ac4bb157a17d455064
Semin-Hong <hongsem0103@gmail.com>
2022-04-28 13:29:20 +0900

    Create Project

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://github.com/ParkYoungWoong"
+      target="_black">
+      (c){{ new Date().getFullYear() }} HEROPY
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from "~/components/Logo";
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+  footer {
+    padding: 70px;
+    text-align: center;
+    opacity: .3;
+    .logo {
+      display: block;
+      margin-bottom: 4px;
+    }
+  }
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SEMIN',
+    email: 'michsel@naver.com',
+    blog: 'https://heropy.com',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
